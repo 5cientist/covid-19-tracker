f94570174596220f03b0666e88e4d85a4ee499fd
hanshir <hanshirlukku@gmail.com>
2020-08-13 17:57:24 +0530

    useState state added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React,{useState}  from "react";
 import { MenuItem, FormControl, Select,
 } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries,setCountries] = useState(["USA","IN","UK"]);
+
+
+  //STATE is how to write a variable in REACT <<<<<<<
+  //USEEFFECT which run a pices of code based on given condition
   return (
     <div className="app">
-      <h1>Scientist <span>🚀</span> </h1>
-      <h1>COVID-19 TRACKER</h1>
+      <div className="app__header">
+      {/* <h1>Scientist <span>🚀</span></h1> */}
+      <h1> COVID-19 TRACKER </h1>
 
-      <FormControl className="app__dropdown">
+{/* i want loops to  countries
+    and show the drop 
+    for that we use state 
+*/}
+
+
+
+
+       <FormControl className="app__dropdown">
         <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">worldwide</MenuItem>
+          {
+           countries.map(country => (
+           <MenuItem value={country}>{country}</MenuItem>
+           ) )
+          }
+          {/* <MenuItem value="worldwide">worldwide</MenuItem>
           <MenuItem value="worldwide">Option two</MenuItem>
           <MenuItem value="worldwide">option 3</MenuItem>
-          <MenuItem value="worldwide">yo yo yo</MenuItem>
+          <MenuItem value="worldwide">yo yo yo</MenuItem> */}
         </Select>
-      </FormControl>
+      </FormControl> 
+      </div>
     </div>
   );
 }
