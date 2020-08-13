f83335724e4a846ca28a686d98ca20e21596fce9
hanshir <hanshirlukku@gmail.com>
2020-08-13 17:10:10 +0530

    added form contrl from material ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-
+import { MenuItem, FormControl, Select,
+} from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build Covid 19 Tracker with 🚀 Scientist</h1>
+      <h1>Scientist <span>🚀</span> </h1>
+      <h1>COVID-19 TRACKER</h1>
 
-      
-
-
-
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">worldwide</MenuItem>
+          <MenuItem value="worldwide">Option two</MenuItem>
+          <MenuItem value="worldwide">option 3</MenuItem>
+          <MenuItem value="worldwide">yo yo yo</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
